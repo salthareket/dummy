1ed519712faa13bc7aea242714e431681799b5f7
salthareket <info@salthareket.com>
2025-02-02 14:40:26 +0300

    1.2.5

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ if (file_exists(__DIR__ . "/vendor/salthareket/theme/src/theme.php")) {
     add_filter("template_include", function ($template) {
        return get_template_directory() . '/static/no-theme.html';
     });
-}  
+}
 
 ini_set('max_execution_time', 1000);
 //remove_action("shutdown", "wp_ob_end_flush_all", 1);
